1c0cffc9dcb33e8d588a1cf13af70f2001541c15
Wei Jin <waynejin0110@gmail.com>
2022-07-31 01:07:37 -0400

    abc

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -40,11 +40,14 @@ let overlays = {
 // Then we add a control to the map that will allow the user to change
 // which layers are visible.
 L.control.layers(baseMaps, overlays).addTo(map);  
+
+
 // Accessing the Toronto neighborhoods GeoJSON URL.
 let torontoHoods = "https://raw.githubusercontent.com/jinwei1207/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
 // Pass our map layers into our layers control and add the layers control to the map.
     L.control.layers(baseMaps).addTo(map);
+
   
   // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
@@ -109,4 +112,37 @@ L.geoJSON(data, {
 }).addTo(earthquakes);
 
 earthquakes.addTo(map);
+
+  
 });
+// Create a legend control object.
+let legend = L.control({
+    position: "bottomright"
+  });
+  // Then add all the details for the legend.
+  legend.onAdd = function() {
+      let div = L.DomUtil.create("div", "info legend");
+      const magnitudes = [0, 1, 2, 3, 4, 5];
+      const colors = [
+        "#98ee00",
+        "#d4ee00",
+        "#eecc00",
+        "#ee9c00",
+        "#ea822c",
+        "#ea2c2c"
+      ];
+      // Looping through our intervals to generate a label with a colored square for each interval.
+  for (var i = 0; i < magnitudes.length; i++) {
+    console.log(colors[i]);
+    div.innerHTML +=
+      "<i style='background: " + colors[i] + "'></i> " +
+      magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+ }
+  return div;
+
+};
+legend.addTo(map);
+    
+   
+  
+
